6c1395b2bbd6dfe6825795fee1dc5aa984e835e9
Valid <100527555+dgssdagdg@users.noreply.github.com>
2024-01-29 17:40:32 +0300

    add

---
+++ js/script.js
@@ -0,0 +1,74 @@
+document.addEventListener('click', function(e) {
+    let menuBtn = document.querySelector('.menu-btn');
+    let menu = document.querySelector('.menu');
+    let header = document.querySelector('.header');
+    let body = document.body
+    if(e.target.closest('.menu-btn')) {
+        menuBtn.classList.toggle('active');
+        menu.classList.toggle('active');
+        header.classList.toggle('header-active')
+        body.classList.toggle('_overflow')
+    } else if (menu.closest('.active') && !e.target.closest('.menu') && menuBtn.closest('.active')) {
+        menuBtn.classList.remove('active');
+        menu.classList.remove('active');
+        header.classList.remove('header-active')
+        body.classList.remove('_overflow')
+    }
+
+    if(e.target.closest('.events-view')) {
+      let event = document.querySelector('.events-view-active')
+      let eventItem = document.querySelector('.events-items-active')
+      let dataJSValue = e.target.getAttribute('data-id');
+      let dataItem = document.getElementById(`${dataJSValue}`);
+      e.target.classList.add('events-view-active')
+      dataItem.classList.add('events-items-active')
+      eventItem.classList.remove('events-items-active')
+      event.classList.remove('events-view-active')
+    }
+
+})
+
+window.onscroll = function(){
+  const html = document.documentElement, body = document.body;
+  const wrapper = document.querySelector('.wrapper')
+  if(html.scrollTop>100||body.scrollTop>100) {
+    wrapper.classList.add('wrapper-active')
+  } else wrapper.classList.remove('wrapper-active')
+}
+
+var t;
+function up() {
+	var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+	if(top > 0) {
+		window.scrollBy(0,-100);
+		t = setTimeout('up()',20);
+	} else clearTimeout(t);
+	return false;
+}
+
+const swiper = new Swiper('.partners-swipers', {
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 30,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.partners-button-next',
+      prevEl: '.partners-button-prev',
+    },
+
+    breakpoints: {
+
+        320: {
+          slidesPerView: 'auto',
+          spaceBetween: 15
+        },
+        767: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        },
+        998: {
+          slidesPerView: 4,
+        }
+      }
+  });
